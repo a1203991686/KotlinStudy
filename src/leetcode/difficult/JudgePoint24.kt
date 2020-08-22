78961f279510e6ce818e8622284069cc8a585428
a1203991686 <a1203991686@126.com>
2020-08-22 09:26:37 +0800

    update:2020/8/22

---
+++ src/leetcode/difficult/JudgePoint24.kt
@@ -0,0 +1,12 @@
+package leetcode.difficult
+
+/**
+ * 679. 24 点游戏
+ * https://leetcode-cn.com/problems/24-game/
+ * @author littlecorgi
+ * @date 2020-04-03 23:41
+ */
+
+fun judgePoint24(nums: IntArray): Boolean {
+    return false
+}
