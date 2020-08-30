7b2693799c2d0065893a8bb3f0ab206532123dc8
a1203991686 <a1203991686@126.com>
2020-08-30 10:08:42 +0800

    update:2020/8/30

---
+++ src/leetcode/easy/ReverseWordsII.kt
@@ -0,0 +1,22 @@
+package leetcode.easy
+
+import java.lang.StringBuilder
+
+/**
+ * 557. 反转字符串中的单词 III
+ * https://leetcode-cn.com/problems/reverse-words-in-a-string-iii/
+ * @author littlecorgi
+ * @date 2020-04-03 23:41
+ */
+// fun reverseWords(s: String): String {
+//     val array = s.split(' ')
+//     val result = StringBuilder()
+//     for (i in array.indices) {
+//         if (i == array.size - 1) {
+//             result.append(array[i].reversed())
+//         } else {
+//             result.append("${array[i].reversed()} ")
+//         }
+//     }
+//     return result.toString()
+// }
